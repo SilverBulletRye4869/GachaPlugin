ecf89dfaca09bedc3981e38419bd233db459d21d
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-02 21:09:23 +0900

    初期設定

---
+++ src/main/java/silverassist/gachaplugin/GachaPlugin.java
@@ -0,0 +1,27 @@
+package silverassist.gachaplugin;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+import silverassist.gachaplugin.mainSystem.Setup;
+
+public final class GachaPlugin extends JavaPlugin {
+
+    private static JavaPlugin plugin;
+    private static CustomConfig dataYml;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        plugin = this;
+        dataYml = new CustomConfig(this,"data.yml");
+        new Command(this, new Setup(this));
+    }
+
+    public static JavaPlugin getInstance() {return plugin;}
+    public static CustomConfig getDataYml() {return dataYml;}
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
