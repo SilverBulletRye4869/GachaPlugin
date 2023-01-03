3500505b94bc8eda20845f41f77fa25c1c5876db
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-03 21:09:45 +0900

    create, editコマンド追加

--- src/main/java/silverassist/gachaplugin/Command.java
+++ src/main/java/silverassist/gachaplugin/Command.java
@@ -7,9 +7,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.gachaplugin.mainSystem.Setup;
 import silverassist.gachaplugin.mainSystem.Spin;
+import silverassist.gachaplugin.menu.admin.ItemList;
+
+import static silverassist.gachaplugin.Util.sendPrefixMessage;
 
 public class Command implements CommandExecutor {
-    final private JavaPlugin plugin;
+    private final JavaPlugin plugin;
+    private final CustomConfig dataYml = GachaPlugin.getDataYml();
     private final Setup GACHA;
 
     public Command(JavaPlugin plugin, Setup gacha){
@@ -20,23 +24,32 @@ public class Command implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-        if(!sender.isOp())return true;
-        try{
-            if(!(sender instanceof Player))return true;
-            Player p = (Player) sender;
-            switch (args[0]){
-                case "test":
-                    if(Spin.isPlay(p))return true;
-                    GACHA.getGacha("test").run(p);
+        if(!sender.isOp() || !(sender instanceof Player) || args.length == 0)return true;
 
-                case "start":
-                    Player runner = args.length==3 ? Bukkit.getPlayer(args[1]) : p;
-                    GACHA.getGacha(args[1]).run(runner);
-
-            }
-
-        }catch (ArrayIndexOutOfBoundsException e){
-            sender.sendMessage("§c引数が足りません！");
+        Player p = (Player) sender;
+        String id = null;
+        if(args.length>0)id = args[1];
+        switch (args[0]){
+            case "test":
+                if(Spin.isPlay(p))return true;
+                GACHA.getGacha("test").run(p);
+                return true;
+            case "start":
+                if(args.length < 2){
+                    sendPrefixMessage(p,"§cガチャを指定してください");
+                    return true;
+                }
+                Player runner = args.length==3 ? Bukkit.getPlayer(args[2]) : p;
+                GACHA.getGacha(id).run(runner);
+                return true;
+            case "create":
+                if(dataYml.getConfig().get(id)!=null){
+                    sendPrefixMessage(p,"§cそのガチャは既に存在します");
+                    return true;
+                }
+                dataYml.getConfig().set(id,"unsetted");
+            case "edit":
+                new ItemList(p,id).open();
         }
         return true;
     }
