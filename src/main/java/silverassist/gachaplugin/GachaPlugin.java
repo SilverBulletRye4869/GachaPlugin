d95849a2b7f49c31011e53885815fd152c490ca8
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-04 20:41:41 +0900

    data.ymlの削除(保存方法変更)

--- src/main/java/silverassist/gachaplugin/GachaPlugin.java
+++ src/main/java/silverassist/gachaplugin/GachaPlugin.java
@@ -1,26 +1,27 @@
 package silverassist.gachaplugin;
 
 
+
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.gachaplugin.mainSystem.Setup;
+
 
 public final class GachaPlugin extends JavaPlugin {
 
     private static JavaPlugin plugin;
-    private static CustomConfig dataYml;
+
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
-        dataYml = new CustomConfig(this,"data.yml");
-        dataYml.saveDefaultConfig();
-
+        this.saveDefaultConfig();
         new Command(this, new Setup(this));
     }
 
     public static JavaPlugin getInstance() {return plugin;}
-    public static CustomConfig getDataYml() {return dataYml;}
+
+
 
     @Override
     public void onDisable() {
