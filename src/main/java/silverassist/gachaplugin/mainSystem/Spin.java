420ffa7d214241e1a76aa1f94b4d2e89e4d89bc4
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-05 20:07:07 +0900

    moneyとItem格納

--- src/main/java/silverassist/gachaplugin/mainSystem/Spin.java
+++ src/main/java/silverassist/gachaplugin/mainSystem/Spin.java
@@ -2,10 +2,13 @@ package silverassist.gachaplugin.mainSystem;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import silverassist.gachaplugin.CustomConfig;
 import silverassist.gachaplugin.GachaPlugin;
 import silverassist.gachaplugin.Util;
 
@@ -19,21 +22,28 @@ public class Spin {
     private static final int LOOP_NUM = 100;
     private static final ItemStack BG = Util.createItem(Material.LIGHT_BLUE_STAINED_GLASS_PANE,"§r");
     private static final ItemStack HIT = Util.createItem(Material.YELLOW_STAINED_GLASS_PANE,"§r");
+    private static final JavaPlugin plugin = GachaPlugin.getInstance();
     private static Set<Player> playing = new HashSet<>();
     private static Set<Player> openingGachaGUI = new HashSet<>();
 
     private final String ID;
     private TreeMap<Integer,ItemStack> gachaData;
     private final int RANDOM_MAX;
+    private final int PAYMENT_MONEY;
+    private final ItemStack PAYMENT_ITEM;
 
     public Spin(String id,TreeMap<Integer,ItemStack> gachaData){
         this.ID = id;
         this.gachaData = gachaData;
         this.RANDOM_MAX = gachaData.lastKey();
+        YamlConfiguration y = CustomConfig.getYmlByID(id);
+        PAYMENT_MONEY = y.getInt("money");
+        PAYMENT_ITEM = y.getItemStack("item");
     }
 
 
     public void run(Player p){
+        p.closeInventory();
         playing.add(p);
         openingGachaGUI.add(p);
         Inventory inv = Bukkit.createInventory(p,27,"gacha-"+ID);
@@ -43,7 +53,13 @@ public class Spin {
         }
         inv.setItem(4,HIT);
         inv.setItem(22,HIT);
-        p.openInventory(inv);
+        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+                    @Override
+                    public void run() {
+                        p.openInventory(inv);
+                    }
+        },1);
+
 
         Bukkit.getScheduler().runTaskAsynchronously(GachaPlugin.getInstance(), new Runnable() {
             @Override
@@ -60,16 +76,17 @@ public class Spin {
                 //ぐるぐる回す
                 for(int i = 0;i<LOOP_NUM;i++) {
                     p.playSound(p.getLocation(),"block.anvil.break",1,1);
-                    InventoryView inv_c = p.getOpenInventory();
-                    for (int j = 0; j < 8; j++) inv_c.setItem(9 + j, inv_c.getItem(10 + j));
-
-                    if(i==LOOP_NUM-5)inv_c.setItem(17,bingoItem);  //最後に留まる位置の調整
-                    else if(isOpen(p)){
-                        int next = (int) (Math.random() * RANDOM_MAX);
-                        for (int j : gachaData.keySet()) {
-                            if (next >= j) continue;
-                            inv_c.setItem(17, gachaData.get(j));
-                            break;
+                    if(isOpen(p)){
+                        InventoryView inv_c = p.getOpenInventory();
+                        for (int j = 0; j < 8; j++) inv_c.setItem(9 + j, inv_c.getItem(10 + j));
+                        if(i==LOOP_NUM-5)inv_c.setItem(17,bingoItem);  //最後に留まる位置の調整
+                        else {
+                            int next = (int) (Math.random() * RANDOM_MAX);
+                            for (int j : gachaData.keySet()) {
+                                if (next >= j) continue;
+                                inv_c.setItem(17, gachaData.get(j));
+                                break;
+                            }
                         }
                     }
                     try {
@@ -94,6 +111,9 @@ public class Spin {
             }
         });
     }
+
+    public int getPaymentMoney(){return this.PAYMENT_MONEY;}
+    public ItemStack getPaymentItem(){return this.PAYMENT_ITEM;}
 
     public static boolean isPlay(Player p){return playing.contains(p);}
 
