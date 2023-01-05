985edcc73168f22d387271b0cd7a071e6deff41c
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-05 20:13:24 +0900

    VaultAPIの対応

--- src/main/java/silverassist/gachaplugin/GachaPlugin.java
+++ src/main/java/silverassist/gachaplugin/GachaPlugin.java
@@ -1,27 +1,39 @@
 package silverassist.gachaplugin;
 
-
-
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.gachaplugin.mainSystem.Setup;
+import silverassist.gachaplugin.menu.user.GachaOpen;
 
 
 public final class GachaPlugin extends JavaPlugin {
 
     private static JavaPlugin plugin;
-
+    private static Setup GACHA_SYSTEM;
+    private static GachaOpen GACHA_OPEN;
+    private static Vault vault = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
         this.saveDefaultConfig();
-        new Command(this, new Setup(this));
+
+        vault = new Vault(this);
+        if (!vault.setupEconomy() ) {
+            vault.log.severe(String.format("[%s] プラグイン「Vault」「Essentials」の認証に失敗しました。", getDescription().getName()));
+        }
+
+        GACHA_SYSTEM = new Setup(this);
+        GACHA_OPEN = new GachaOpen(this);
+        new Command(this);
+
     }
 
     public static JavaPlugin getInstance() {return plugin;}
-
-
+    public static Setup getGachaSystem(){ return GACHA_SYSTEM;}
+    public static GachaOpen getGachaOpen(){return GACHA_OPEN;}
+    public static Vault getVault(){return vault;}
+    public static boolean existVault(){return  Vault.getEconomy()!=null;}
 
     @Override
     public void onDisable() {
---
+++ src/main/java/silverassist/gachaplugin/Vault.java
@@ -0,0 +1,33 @@
+package silverassist.gachaplugin;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
+
+public class Vault{
+    private JavaPlugin plugin;  //PluginInstanceを格納しておく奴
+    static final Logger log = Logger.getLogger("Minecraft");  //Log登録
+    private static Economy econ = null;  //お金をいじるのに必要な奴を入れておくところ
+
+    //コンストラクタ
+    public Vault(JavaPlugin plugin){
+        this.plugin = plugin;
+    }
+
+    //お金系のシステム起動
+    boolean setupEconomy() {
+        if (plugin.getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = this.plugin.getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+    public static Economy getEconomy() {
+        return econ;
+    }  //お金をいじるのに必要な奴
+}
