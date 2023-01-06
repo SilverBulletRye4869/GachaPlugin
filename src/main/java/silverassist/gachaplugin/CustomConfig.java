f1c99c69f77d1f99dba74f2d6392e025270252b0
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-01-06 17:01:03 +0900

    createYml関数追加

--- src/main/java/silverassist/gachaplugin/CustomConfig.java
+++ src/main/java/silverassist/gachaplugin/CustomConfig.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 
@@ -12,27 +13,38 @@ public class CustomConfig {
 
     private static Map<String, YamlConfiguration> config = new HashMap<>();
     private static JavaPlugin plugin = GachaPlugin.getInstance();
+    private static HashSet<String> existSet = new HashSet<>();
 
     public static YamlConfiguration getYmlByID(String id) {
-        if(!config.containsKey(id))reloadYmlByID(id);
+        if(!config.containsKey(id)){
+            if(!reloadYmlByID(id))return null;
+        }
         return config.get(id);
     }
 
     public static boolean existYml(String id){
-        return new File(plugin.getDataFolder(),"data/"+id+".yml").exists();
+        if(existSet.contains(id))return true;
+        if(new File(plugin.getDataFolder(),"data/"+id+".yml").exists()){existSet.add(id);return true;}
+        return false;
     }
 
-    public static void reloadYmlByID(String id){
-        File file = new File(plugin.getDataFolder(),"gacha_"+id+".yml");
-        if(!file.exists()){
-            try {
-                file.createNewFile();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
+    public static YamlConfiguration createYmlByID(String id){
+        File file = new File(plugin.getDataFolder(),"data/"+id+".yml");
+        try {
+            file.createNewFile();
+        }catch (IOException e){
+            System.err.println("id: "+id+"のymlファイルの作成に失敗しました");
+            return null;
         }
+        return getYmlByID(id);
+    }
+
+    public static boolean reloadYmlByID(String id){
+        File file = new File(plugin.getDataFolder(),"data/"+id+".yml");
+        if(!file.exists())return false;
         YamlConfiguration y = YamlConfiguration.loadConfiguration(file);
         config.put(id,y);
+        return true;
     }
 
     public static void saveYmlByID(String id){
